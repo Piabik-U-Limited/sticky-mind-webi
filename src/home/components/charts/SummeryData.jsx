760d7eff11fd9344bd4accd342abb00cef3e8509
unknown <jumajosephat61@gmail.com>
2024-01-22 11:58:38 +0300

    Increase stats card height

--- src/home/components/charts/SummeryData.jsx
+++ src/home/components/charts/SummeryData.jsx
@@ -12,7 +12,7 @@ function SummeryData() {
       <Grid item xs={12} sm={6} md={3}>
         <Card
           className="card-content"
-          sx={{ borderRadius: 1, backgroundColor: "purple", padding: 1 }}
+          sx={{ borderRadius: 1, backgroundColor: "purple", padding: 1, height:'100%' }}
         >
           {state.loading ? (
             <PulseLoader color="#fff" height={50} width={15} />
@@ -42,7 +42,7 @@ function SummeryData() {
       <Grid item xs={12} sm={6} md={3}>
         <Card
           className="card-content"
-          sx={{ borderRadius: 1, backgroundColor: "#87CEEB", padding: 1 }}
+          sx={{ borderRadius: 1, backgroundColor: "#87CEEB", padding: 1, height:'100%' }}
         >
           {state.loading ? (
             <PulseLoader color="#fff" height={50} width={15} />
@@ -76,6 +76,7 @@ function SummeryData() {
             borderRadius: 1,
             backgroundColor: "#0F9D58",
             padding: 1,
+            height:'100%'
           }}
         >
           {state.loading ? (
@@ -94,7 +95,7 @@ function SummeryData() {
               <div>
                 <Typography variant="subtitle1">Gross Sales</Typography>
                 <Typography variant="h6">
-                  UGX. {state.dashboardData?.netIncome}
+                  UGX. {state.dashboardData?.grossSales}
                 </Typography>
               </div>
             </div>
@@ -110,6 +111,7 @@ function SummeryData() {
             borderRadius: 1,
             backgroundColor: "#FFBB28",
             padding: 1,
+            height:'100%'
           }}
         >
           {state.loading ? (
