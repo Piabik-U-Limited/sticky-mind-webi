a4bf7bd4e4fd255bad562b33630ba783e1205c88
unknown <jumajosephat61@gmail.com>
2024-01-23 01:44:42 +0300

    Show stat type

--- src/home/components/Summary.jsx
+++ src/home/components/Summary.jsx
@@ -1,20 +1,28 @@
 import React from "react";
-import { Typography, Box, Card, Grid,FormControl,InputLabel,Select,MenuItem } from "@mui/material";
+import {
+  Typography,
+  Box,
+  Card,
+  Grid,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
 import "./styles/summery.css";
 import { setStatType } from "../../redux/slices/dashbard.slice";
-import { useSelector, useDispatch} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { SummeryBarChart, SummeryPieChart, SummeryData } from "./charts";
 import useDashboard from "../../api/hooks/useDashboard";
 function Summary() {
   const dispatch = useDispatch();
   const { handleFetchStats } = useDashboard();
-  const statType = useSelector((state) => state.dashboard.statsType);
+  const statType = useSelector((state) => state.dashboard.statType);
 
   const handleFetchStatsType = async (value) => {
     dispatch(setStatType(value));
     handleFetchStats(value);
-    
-  }
+  };
   return (
     <div>
       <SummeryData />
@@ -22,28 +30,25 @@ function Summary() {
         <Grid container className="graphWrapper">
           <div className="lineGraph">
             <div className="chart-heading" style={{ padding: 10 }}>
-              <Typography style={{ fontWeight: "600" }} variant="h6">
-                Monthly Sales
+              <Typography style={{ fontWeight: "600" }} variant="h6" textTransform={"capitalize"}>
+                {statType} Sales
               </Typography>
-              <FormControl
-          sx={{ minWidth: 120 }}
-          size="small"
-        >
-          <InputLabel id="toggle">Stat Type</InputLabel>
-          <Select
-            labelId="toggle"
-            value={statType}
-            label="Statstics"
-            onChange={(e) => handleFetchStatsType(e.target.value)}
-            sx={{}}
-            size="small"
-          >
-            <MenuItem value={"hourly"}>Hourly</MenuItem>
-            <MenuItem value={"daily"}>Daily</MenuItem>
-            <MenuItem value={"weekly"}>Weekly</MenuItem>
-            <MenuItem value={"monthly"}>Monthly</MenuItem>
-          </Select>
-        </FormControl>
+              <FormControl sx={{ minWidth: 120 }} size="small">
+                <InputLabel id="toggle">Stat Type</InputLabel>
+                <Select
+                  labelId="toggle"
+                  value={statType}
+                  label="Statstics"
+                  onChange={(e) => handleFetchStatsType(e.target.value)}
+                  //sx={{color:'#0F9D58'}}
+                  size="small"
+                >
+                  <MenuItem value={"hourly"}>Hourly</MenuItem>
+                  <MenuItem value={"daily"}>Daily</MenuItem>
+                  <MenuItem value={"weekly"}>Weekly</MenuItem>
+                  <MenuItem value={"monthly"}>Monthly</MenuItem>
+                </Select>
+              </FormControl>
             </div>
             <SummeryBarChart />
           </div>
@@ -55,7 +60,9 @@ function Summary() {
             }}
           >
             <div>
-              <Typography variant="h6">Sales</Typography>
+            <Typography style={{ fontWeight: "600" }} textTransform={"capitalize"}>
+                 Sales Pie-Chart
+              </Typography>
             </div>
             <div>
               <SummeryPieChart />
