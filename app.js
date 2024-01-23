9e26088763143b18dccce40f602a510e97f4434b
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2024-01-23 22:31:47 +0300

    newerconfigs

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const path = require("path");
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+  app.use(express.static("dist"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "dist", "index.html"));
   });
 }
 
