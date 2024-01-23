3e4afafe9006c6c95cbb06c1cb6570ae1fbc6905
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2024-01-23 22:40:15 +0300

    Fixed issues in server configs

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const path = require("path");
 
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("dist"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "dist", "index.html"));
-  });
-}
+//if (process.env.NODE_ENV === "production") {
+app.use(express.static("dist"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
+//}
 
 app.listen(port, (err) => {
   if (err) return console.log(err);
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 
 app.use(compression());
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
