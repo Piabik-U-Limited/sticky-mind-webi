cfdea46c48b7716a02b95c17b97a6e3c859244b4
Juma Josephat <jumajosephat61@gmail.com>
2024-01-26 13:51:10 +0300

    Change piechart

--- src/home/components/charts/SummeryPieChart.jsx
+++ src/home/components/charts/SummeryPieChart.jsx
@@ -14,6 +14,7 @@ import {
   Line,
 } from "recharts";
 import { useSelector } from "react-redux";
+import { Card, Grid } from "@mui/material";
 const SummeryPieChart = () => {
   // const data = [
   //   { name: "January", sales: 400 },
@@ -24,29 +25,56 @@ const SummeryPieChart = () => {
   //   { name: "June", sales: 800 },
   // ];
 
-  const data= useSelector((state) => state.dashboard.stats);
+  const data = useSelector((state) => state.dashboard.stats);
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
   const COLORS = ["purple", "#87CEEB", "#FFBB28", "#0F9D58", "yellow", "blue"];
   return (
-    <PieChart width={200} height={200}>
-      <Pie
-        data={data}
-        cx="50%"
-        cy="50%"
-        labelLine={true}
-        label={"Data"}
-        outerRadius={50}
-        fill="#8884d8"
-        dataKey="sales"
-      >
-        {data.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
+    <Grid container spacing={2} alignItems="center" justifyContent={"center"}>
+      <PieChart width={200} height={200}>
+        <Pie
+          data={data}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey="sales"
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
 
-      <Tooltip />
-      <Legend />
-    </PieChart>
+        <Tooltip />
+       
+      </PieChart>
+    </Grid>
   );
 };
 
